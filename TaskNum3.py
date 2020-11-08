e1567f5db386bc859ccedddd8316383424c80362
AlexndrV59 <kent1234810@gmail.com>
2020-11-08 13:09:34 +0500

    fix TaskNum3.py

--- TaskNum3.py
+++ TaskNum3.py
@@ -1,4 +1,4 @@
-list_one = ['Alexandr','Anastasia']
-list_second = [21,22]
+list_one = ['Ryan Rodney Reynolds','Matthew McConaughey']
+list_second = [44,51]
 dictionary = dict(zip(list_one,list_second)) #Function zip can create a dictionary from two lists, where the key is first_list, and value second_list
 print(dictionary)
